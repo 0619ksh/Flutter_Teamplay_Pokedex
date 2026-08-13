59979691c1d83cf70a34a42ed624744e20408ac1
SeunghyunKim <0619ksh23@gmail.com>
2026-08-13 13:49:42 +0900

    comment

--- lib/model/region.dart
+++ lib/model/region.dart
@@ -1,10 +1,12 @@
 import 'package:pokedex_app/model/pokemon.dart';
 
 class Region {
+  // Properties
   String name;
   String image;
   List<Pokemon> pokemonList;
 
+  // Constructor
   Region(
     {
       required this.name,
--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,7 +1,9 @@
 class User {
+  // Properties
   String id;
   String password;
 
+  // Constructor
   User(
     {
       required this.id,
