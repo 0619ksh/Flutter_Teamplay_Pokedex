ded8c3325760c180cac45b88d983e9d93f186aa8
SeunghyunKim <0619ksh23@gmail.com>
2026-08-13 14:09:58 +0900

    asdf

---
+++ lib/view/list_view.dart
@@ -0,0 +1,18 @@
+// 지방 별 포켓몬 목록
+import 'package:flutter/material.dart';
+
+class ListView extends StatefulWidget {
+  const ListView({super.key});
+
+  @override
+  State<ListView> createState() => _ListViewState();
+}
+
+class _ListViewState extends State<ListView> {
+  // Properties
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
